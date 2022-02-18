e260fac968d1b79dbf5f94327b0f109d8158f0d5
Sergio Falcon <SergioJFalcon@gmail.com>
2022-02-18 10:46:43 -0600

    wip

--- frontend/app/src/pages/HomePage.js
+++ frontend/app/src/pages/HomePage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+
 // import Product from "../components/Product";
 // import products from '../products';
 
@@ -6,6 +8,7 @@ const HomePage = () => {
     return (
         <div>
             <h1>Lastest Products</h1>
+            <Button variant="contained" color="warning">New Button</Button>
             {/* <Row>
                 {products.map(product => (
                     <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
